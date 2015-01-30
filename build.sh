bd012e6178855ba02cb2b14c507b09f380551f35
edunham <edunham@osuosl.org>
2015-01-30 10:27:06 -0800

    less symlinking.

--- build.sh
+++ build.sh
@@ -2,21 +2,16 @@ git pull
 source v/bin/activate
     rm -rf _build
     make slides
-    ln -s _build/slides/index.html index.html
 cd humanhacking
     rm -rf _build
     make slides
-    ln -s _build/slides ../humans
 cd ../dobc
     rm -rf _build
     make slides
-    ln -s _build/slides ../dobc
 cd ../git
     rm -rf _build
     make slides
-    ln -s _build/slides ../git
 cd ../intermediateirc
     rm -rf _build
     make slides
-    ln -s _build/slides ../irc
 deactivate
